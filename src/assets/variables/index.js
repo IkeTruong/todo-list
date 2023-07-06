d865b2662d6b204b51415281b5754092209c713d
Ike Truong <ike.truong@metacrew.vn>
2023-07-06 10:06:31 +0700

    Add variables color

--- src/assets/variables/index.js
+++ src/assets/variables/index.js
@@ -1,5 +1,14 @@
-export const cardShadowHover = '0px 3px 20px 0px rgba(44, 101, 144, 0.3)'
-export const whiteLightColor = 'rgba(0, 0, 0, 0.12)'
-export const priorityOpts = ['high', 'medium', 'low']
+// gradients color
+export const midnightColor = 'linear-gradient(to left, #232526, #414345)'
+export const blueSkiesColor = 'linear-gradient(to top, #56ccf2, #2f80ed)'
+export const lushColor = 'linear-gradient(to right, #56ab2f, #a8e063)'
+export const sunriseColor = 'linear-gradient(to right, #ff512f, #f09819)'
 
+// box shadow
+export const cardShadowHover = '0px 3px 20px 0px rgba(44, 101, 144, 0.3)'
+
+// options
+export const priorityOpts = ['high', 'medium', 'low']
 export const statusOpts = ['todo', 'processing', 'pending', 'done']
+
+export const whiteLightColor = 'rgba(0, 0, 0, 0.12)'
