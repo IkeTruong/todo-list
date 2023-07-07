941e9602c79f690fb45dab0bded5867e2bfe7536
Ike Truong <ike.truong@metacrew.vn>
2023-07-07 13:01:27 +0700

    Create Typo Title

---
+++ src/components/TypoAdvanced/TitleTypography.js
@@ -0,0 +1,14 @@
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+
+export const TitleTypography = styled(Typography)(({ theme }) => ({
+  WebkitBackgroundClip: 'text',
+  backgroundImage:
+    'linear-gradient(to right, #09f1b8, #00a2ff, #ff00d2, #fed90f)',
+  letterSpacing: 'calc(1em / 8)',
+  color: theme.palette.common.white,
+  fontWeight: 700,
+  padding: 'calc(calc(1em / 16) / 2)',
+  '-webkit-text-stroke-color': 'transparent',
+  '-webkit-text-stroke-width': 'calc(1em / 16)',
+}))
